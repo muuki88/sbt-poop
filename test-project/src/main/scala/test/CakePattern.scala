670117a8069c725aa401fa396de3307526bcf191
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-12-02 19:20:11 +0100

    Add setting to disable relative filenames
    Closes GH-1

--- test-project/src/main/scala/test/CakePattern.scala
+++ test-project/src/main/scala/test/CakePattern.scala
@@ -3,33 +3,32 @@ package test
 class ComponentRegistry extends ComponentBastiImpl with ComponentMukiImpl
 
 trait Basti
-trait ComponentBasti { self : ComponentMuki =>
+trait ComponentBasti { self: ComponentMuki =>
   val basti: Basti
 }
-trait ComponentBastiImpl { self : ComponentMuki with FloComponent =>
-  val basti = new Basti { 
-  
+trait ComponentBastiImpl { self: ComponentMuki with FloComponent =>
+  val basti = new Basti {
+
   }
 }
 
-
 trait Muki
 trait ComponentMuki {
-	val muki: Muki
+  val muki: Muki
 }
 trait ComponentMukiImpl {
-	val muki = new Muki { 
-		
-	}
+  val muki = new Muki {
+
+  }
 }
 
 trait Flo
 trait ComponentFlo {
-	val flo: Flo
+  val flo: Flo
 }
 trait ComponentFloImpl {
-	val flo = new Flo { 
-		
-	}
+  val flo = new Flo {
+
+  }
 }
 
